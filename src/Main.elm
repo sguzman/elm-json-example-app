7369993fde6638c68dd3385621719ce118887e39
Salvador Guzman <guzmansalv@gmail.com>
2022-04-03 14:46:24 -0700

    add http request

--- src/Main.elm
+++ src/Main.elm
@@ -1,57 +1,91 @@
+-- Make a GET request to load a book called "Public Opinion"
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/effects/http.html
+--
+
+
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, h2, img, input)
-import Html.Attributes exposing (src)
-import Html.Events exposing (onInput)
-import Html.Attributes exposing (placeholder)
-import Html.Attributes exposing (value)
-import Json
+import Html exposing (Html, pre, text)
+import Http
 
----- MODEL ----
-type alias Model =
-    {
-        content : String
-    }
 
-init : Model
-init =
-    {
-       content = ""
-    }
 
----- UPDATE ----
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type Model
+    = Failure
+    | Loading
+    | Success String
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading
+    , Http.get
+        { url = "http://localhost:9090/people.json"
+        , expect = Http.expectString GotText
+        }
+    )
+
+
+
+-- UPDATE
+
+
 type Msg
-    = Change String
+    = GotText (Result Http.Error String)
 
-update : Msg -> Model -> Model
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Change new ->
-            {
-                model | content = new
-            }
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( Success fullText, Cmd.none )
 
----- VIEW ----
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        [
-         img [ src "/logo.svg" ] [],
-         h1 [] [ text "Your Elm App is working. Yay!" ],
-         h2 [] [ text ("Welcome, Mr " ++ model.content) ],
-         input [
-              placeholder "enter name here",
-              value model.content,
-              onInput Change
-         ] []
-        ]
+    case model of
+        Failure ->
+            text "I was unable to load your book."
 
----- PROGRAM ----
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { view = view
-        , init = init
-        , update = update
-        }
+        Loading ->
+            text "Loading..."
+
+        Success fullText ->
+            pre [] [ text fullText ]
